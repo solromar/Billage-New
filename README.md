f16a9a5432cf42a39f1596bedcc32c898735b3b9
soledad <soledad@smartescrow.es>
2023-10-10 15:12:49 +0000

    add Spendings

---
+++ README.md
@@ -0,0 +1,46 @@
+# Guía de Uso de la API de Billage
+
+Esta guía te proporcionará información detallada sobre cómo utilizar la API de Billage (API V2.1) para interactuar programáticamente con tus datos de facturación y gastos. La API de Billage te permite acceder y gestionar facturas, gastos y otros datos relacionados con tus transacciones comerciales.
+
+## Introducción
+La API de Billage es una interfaz de programación de aplicaciones que te permite acceder a tus datos de Billage y realizar operaciones como la consulta de facturas, la creación de nuevos gastos y la gestión de clientes. Para utilizar la API, necesitas una clave API válida que puedes obtener desde la plataforma de Billage.
+
+## Base URL
+La URL base para todas las solicitudes a la API de Billage es:
+https://app.getbillage.com/api
+
+## Autenticación
+Para autenticarte en la API de Billage, debes incluir tu clave API en las cabeceras de todas tus solicitudes HTTP 
+
+## Obtener Todas las Facturas de Gastos (Compras)
+
+Se realiza una 1er llamada a GET https://app.getbillage.com/api/v2/spendings para obtener todos los ID's de las facturas y asi poder obtner los detalles de cada factura desde
+GET https://app.getbillage.com/api/v2/spendings
+
+Opcionalmente se puede pasar los siguientes parametros por el Body en la primer llamada
+
+### Parámetros Opcionales
+
+Puedes utilizar varios parámetros opcionales para refinar tu búsqueda, incluyendo:
+
+- `q`: Filtrar por código, fecha u otros detalles.
+- `start`: Posición de inicio para la consulta.
+- `elements`: Número de elementos a recuperar por página.
+- `ref`: Código de referencia.
+- `serie`: Serie de la factura.
+- `account`: Cuenta relacionada con la factura.
+- `colour`: Nombre del color asociado a la factura.
+- `date-from`: Fecha de inicio (en formato yyyy-MM-dd).
+- `date-to`: Fecha de fin (en formato yyyy-MM-dd).
+- `owner`: Propietario de la factura.
+- `state`: Estado de la factura.
+- `category`: Categoría de la factura.
+- `tags`: Etiquetas asociadas a la factura.
+- `summarized`: Obtener resultados resumidos (true o false).
+- `sort`: Campos por los cuales ordenar los resultados.
+
+
+
+
+
+
--- src/Controller/SpendingsController.php
+++ src/Controller/SpendingsController.php
@@ -2,17 +2,104 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use GuzzleHttp\Client;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class SpendingsController extends AbstractController
 {
     #[Route('/spendings', name: 'app_spendings')]
-    public function index(): Response
+    public function getspendings()
     {
-        return $this->render('spendings/index.html.twig', [
-            'controller_name' => 'SpendingsController',
+        $authorizationKey = 'lfoiEtlbkZk5tgOIN2xGEe3xsJdvk71QBT1FwSsSCvBPIYX9b5E5ttcFZBTK13LeTjARSNCtSALydGtNN7Vr8bPhsgzSIqjZasCKpHXq9wu686sZrnk5li4hnJrI9bFr';
+
+        // ------------------------------------------- Autorización -----------------------------------------------------//
+
+        $client = new Client();
+        $response = $client->request('GET', 'https://app.getbillage.com/api/v2/spendings', [
+            'headers' => [
+                'Authorization' => $authorizationKey,
+            ],
         ]);
+        $spendings = json_decode($response->getBody()->getContents(), true);
+
+        // Crear un arreglo para almacenar los detalles de las facturas
+        $detalleFacturas = [];
+
+        foreach ($spendings['page'] as $spending) {
+            // Realizar una solicitud adicional para obtener detalles de la factura por ID
+            $responseDetalle = $client->request('GET', 'https://app.getbillage.com/api/v2/spendings/' . $spending['id'], [
+                'headers' => [
+                    'Authorization' => $authorizationKey,
+                ],
+            ]);
+            $detalleFactura = json_decode($responseDetalle->getBody()->getContents(), true);
+            //---------------------------------------------- ACCESO A ESCTRUCTURAS Y CAMPOS ------------------------------------//
+            //  "retention"
+            $retention = $detalleFactura['retention'];
+            $retentionName = $retention['name'];
+            $retentionValue = $retention['value'];
+            //     "paymentDocument"
+            $paymentDocument = $detalleFactura['payment_document'];
+            $paymentName = $paymentDocument['name'];           
+            //  "provider"
+            $provider = $detalleFactura['provider'];
+            $provider_name = $provider['business_name'];
+            $nif = $provider['vat'];
+
+            // ASIGNACION DE VARIABLES //
+            $factura = [
+                'ID de Factura' => $detalleFactura['id'],
+                'Fecha' => $detalleFactura['date'],
+                'Estado' => $detalleFactura['state'],
+                'Tipo de Factura' => $detalleFactura['type'],
+                'Detalle Proveedor' => [
+                    'Razon Social' => $provider_name,
+                    'NIF' => $nif,
+                ],
+                'Total Base Imponible' => $detalleFactura['base'],
+                'Total Impuestos' => $detalleFactura['taxes'],
+                'Total Factura' => $detalleFactura['total'],
+                'Total Retenciones' => $detalleFactura['retention_amount'],
+                'Detalle Retención' => [
+                    'Tipo retencion' => $retentionName,
+                    'Porcentaje' => $retentionValue,
+                ],
+                'Forma de Pago' => [
+                    'Metodo de Pago' => $paymentName,
+                    
+                ],
+
+            ];
+            // ------------------------------------- PRODUCTOS ------------------------------------------------//
+            $products = [];
+            foreach ($detalleFactura['concepts'] as $product) {
+                $productDetails = [
+                    'Descripción' => $product['description'],
+                    'Cantidad' => $product['quantity'],
+                    'Precio' => $product['price'],
+                    'Descuento' => $product['discount'],
+                    'Total' => $product['total'],
+                    'Impuesto' => [
+                        'Nombre' => $product['tax']['name'],
+                        'Porcentaje' => $product['tax']['value'],
+                        'Etiqueta' => $product['tax']['label'],
+                        'Recargo' => $product['tax']['surcharge'],
+                    ],
+                ];
+                $products[] = $productDetails;
+            }
+            $factura['Productos'] = $products;
+            // Agregar la factura al arreglo de respuesta
+            $detalleFacturas[] = $factura;
+        }
+        // Crear la respuesta final
+        $respuesta = [
+            'CANTIDAD TOTAL DE FACTURAS' => $spendings['total_elements'],
+            'FACTURAS RECIBIDAS EN EL PERIODO' => $detalleFacturas,
+        ];
+        return new JsonResponse($respuesta);
     }
 }
