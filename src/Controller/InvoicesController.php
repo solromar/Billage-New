2f4c04c1f331b787fc72dfddbece64b8b954aee0
soledad <soledad@smartescrow.es>
2023-10-10 15:54:56 +0000

    add Invoices

--- src/Controller/InvoicesController.php
+++ src/Controller/InvoicesController.php
@@ -16,7 +16,7 @@ class InvoicesController extends AbstractController
     {
         $authorizationKey = 'lfoiEtlbkZk5tgOIN2xGEe3xsJdvk71QBT1FwSsSCvBPIYX9b5E5ttcFZBTK13LeTjARSNCtSALydGtNN7Vr8bPhsgzSIqjZasCKpHXq9wu686sZrnk5li4hnJrI9bFr';
 
-        // -------------------------------------------Obtener el token de Autorización -----------------------------------------------------//
+        // -------------------------------------------Autorización -----------------------------------------------------//
         $client = new Client();
         $response = $client->request('GET', 'https://app.getbillage.com/api/v2/invoices', [
             'headers' => [
@@ -24,28 +24,84 @@ class InvoicesController extends AbstractController
             ],
         ]);
         $invoices = json_decode($response->getBody()->getContents(), true);
-        // --------------------------------------- Recorrer las facturas y asignar variables ---------------------------------------------------------//        
-        $cantidadTotalFacturas = $invoices['total_elements'];
-        // Crear un array que contendrá tanto la cantidad total de facturas como las facturas individuales
-        $respuesta = [
-            'CANTIDAD TOTAL DE FACTURAS' => $cantidadTotalFacturas,
-            'FACTURAS EMITIDAS EN EL PERIODO' => [],
-        ];
+
+        // Crear un arreglo para almacenar los detalles de las facturas
+        $detalleFacturas = [];
+
         foreach ($invoices['page'] as $invoice) {
+            // Realizar una solicitud adicional para obtener detalles de la factura por ID
+            $responseDetalle = $client->request('GET', 'https://app.getbillage.com/api/v2/invoices/' . $invoice['id'], [
+                'headers' => [
+                    'Authorization' => $authorizationKey,
+                ],
+            ]);
+
+            $detalleFactura = json_decode($responseDetalle->getBody()->getContents(), true);
+        
+          //---------------------------------------------- ACCESO A ESCTRUCTURAS Y CAMPOS ------------------------------------//
+            //  "retention"
+            $retention = $detalleFactura['retention'];
+            $retentionName = $retention['name'];
+            $retentionValue = $retention['value'];
+            //     "paymentDocument"
+            $paymentDocument = $detalleFactura['payment_document'];
+            $paymentName = $paymentDocument['name'];           
+            //  "customer"
+            $customer = $detalleFactura['customer'];
+            $customerName = $customer['business_name'];
+            $customerNif = $customer['vat'];
+
+            // ASIGNACION DE VARIABLES //
             $factura = [
-                'ID de Factura' => $invoice['id'],
-                'Razon Social Cliente' => $invoice['customer_name'],
-                'NIF Cliente' => $invoice['customer_vat'],
-                'Tipo de Factura' => $invoice['type'],
-                'Fecha de Emision' => $invoice['date'],
-                'Número de Factura' => $invoice['number'],
-                'Estado de la Factura' => $invoice['state'],
-                'Base total imponible' => number_format($invoice['base'], 2, '.', ','),
-                'Importe total de impuestos' => number_format($invoice['taxes'], 2, '.', ','),
-                'Importe total de la factura ' => number_format($invoice['total'], 2, '.', ','),
+                'ID de Factura' => $detalleFactura['id'],
+                'Fecha' => $detalleFactura['date'],
+                'Estado' => $detalleFactura['state'],
+                'Tipo de Factura' => $detalleFactura['type'],
+                'Detalle Cliente' => [
+                    'Razon Social' => $customerName,
+                    'NIF' => $customerNif,
+                ],
+                'Total Base Imponible' => $detalleFactura['base'],
+                'Total Impuestos' => $detalleFactura['taxes'],
+                'Total Factura' => $detalleFactura['total'],
+                'Total Retenciones' => $detalleFactura['retention_amount'],
+                'Detalle Retención' => [
+                    'Tipo retencion' => $retentionName,
+                    'Porcentaje' => $retentionValue,
+                ],
+                'Forma de Pago' => [
+                    'Metodo de Pago' => $paymentName,
+                    
+                ],
+
             ];
-            $respuesta['FACTURAS EMITIDAS EN EL PERIODO'][] = $factura;
+        // ------------------------------------- PRODUCTOS ------------------------------------------------//
+        $products = [];
+        foreach ($detalleFactura['concepts'] as $product) {
+            $productDetails = [
+                'Descripción' => $product['description'],
+                'Cantidad' => $product['quantity'],
+                'Precio' => $product['price'],
+                'Descuento' => $product['discount'],
+                'Total' => $product['total'],
+                'Impuesto' => [
+                    'Nombre' => $product['tax']['name'],
+                    'Porcentaje' => $product['tax']['value'],
+                    'Etiqueta' => $product['tax']['label'],
+                    'Recargo' => $product['tax']['surcharge'],
+                ],
+            ];
+            $products[] = $productDetails;
         }
-        return new JsonResponse($respuesta);
+        $factura['Productos'] = $products;
+        // Agregar la factura al arreglo de respuesta
+        $detalleFacturas[] = $factura;
     }
+    // Crear la respuesta final
+    $respuesta = [
+        'CANTIDAD TOTAL DE FACTURAS' => $invoices['total_elements'],
+        'FACTURAS RECIBIDAS EN EL PERIODO' => $detalleFacturas,
+    ];
+    return new JsonResponse($respuesta);
+}
 }
